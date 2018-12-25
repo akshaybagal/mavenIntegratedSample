e96b4dc53f1fa31cc743cfe41cd833dc06632ed1
Akshay <Akshay@DESKTOP-HFD38AQ>
2018-12-25 22:34:46 +0530

    Change the Path names.

--- src/main/java/com/mycompany/mavensample/GenericResource.java
+++ src/main/java/com/mycompany/mavensample/GenericResource.java
@@ -13,13 +13,12 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
-import javax.xml.bind.JAXBElement;
 /**
  * REST Web Service
  *
  * @author Akshay
  */
-@Path("gen")
+@Path("generic")
 public class GenericResource {
 
     @Context
